6cc87c81047fbba7043175822bacfdc80c442ae1
Victor Costa <vfcosta@gmail.com>
2014-04-16 09:46:44 -0300

    community-hub: improve listener

--- plugins/community_hub/lib/community_hub_plugin/hub.rb
+++ plugins/community_hub/lib/community_hub_plugin/hub.rb
@@ -34,33 +34,6 @@ class CommunityHubPlugin::Hub < Folder
     true
   end
 
-  def twitter_service
-    Twurl::Stream.run(self, nil, hashtags_twitter, File.dirname(__FILE__) + '/../../tweeter_stream/config/twurlrc', proxy_url)
-  end
-
-  def facebook_service
-    facebook_comments(self, nil, facebook_page_id, facebook_pooling_time, facebook_access_token, proxy_url)
-  end
-
-  def self.start_listen
-    hubs = {}
-    loop do
-      puts "searching for hubs"
-      CommunityHubPlugin::Hub.all.each do |hub|
-        hub_conf = hubs[hub.id]
-        if hub_conf.nil? || hub_conf[:hub].updated_at < hub.updated_at
-          hub_conf[:threads].each {|t| t.terminate} if hub_conf
-          puts "hub #{hub.id} found!!!!!!"
-          threads = []
-          threads << Thread.new { hub.twitter_service } if hub.twitter_enabled
-          threads << Thread.new { hub.facebook_service } if hub.facebook_enabled
-          hubs[hub.id] = {:threads => threads, :hub => hub}
-        end
-      end
-      sleep(5)
-    end
-  end
-
   def view_page
     "content_viewer/hub.rhtml"
   end
