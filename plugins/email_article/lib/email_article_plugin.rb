e2b501d83cf90cd34c07d455e242aea6d819f04e
Evandro Junior <evandrojr@gmail.com>
2014-05-08 21:16:36 +0000

    Send an article by email to members of a community

---
+++ plugins/email_article/lib/email_article_plugin.rb
@@ -0,0 +1,30 @@
+class EmailArticlePlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    "Email Article to Community Members Plugin"
+  end
+
+  def self.plugin_description
+    _("A plugin that emails an article to the members of the community.")
+  end
+
+  def article_toolbar_extra_buttons
+    label = _("Send article to members")
+    htmlclass = _("button with-text icon-menu-mail")
+    title = _("Email article to all community members")
+      lambda {
+        link_to_remote(
+            label,
+            {
+               :url => { :controller => 'email_article_plugin_myprofile', :action => "send_email", :id => @page},
+               :method => :get,
+               :success => "alert('Emails enviados')",
+               :failure => "alert('Erro ao enviar email')",
+               :confirm => _("Are you sure you want to email this article to the all community members?"),
+            },
+            :class => htmlclass,
+            :title => title
+         )
+      }
+  end
+end
---
+++ plugins/email_article/test/unit/email_article_plugin_test.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class EmailArticlePluginTest < ActiveSupport::TestCase
+
+  def setup
+    @plugin = EmailArticlePlugin.new
+  end
+
+  should 'be a noosfero plugin' do
+    assert_kind_of Noosfero::Plugin, @plugin
+  end
+
+  should 'have name' do
+    assert_equal 'Relevant Content Plugin', EmailArticlePlugin.plugin_name
+  end
+
+  should 'have description' do
+    assert_equal  _("A plugin that lists the most accessed, most commented, most liked and most disliked contents."), EmailArticlePlugin.plugin_description
+  end
+
+end
