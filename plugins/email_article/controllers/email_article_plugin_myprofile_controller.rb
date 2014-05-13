dce23d0cba870e737bbe743f689f63d323ed2205
Evandro Magalhaes Leite Junior - SUPDE/DEPAE/DE009 <93274300500@serpro-1553624.(none)>
2014-05-13 17:54:58 -0300

    Deleted unnecessary view

--- plugins/email_article/controllers/email_article_plugin_myprofile_controller.rb
+++ plugins/email_article/controllers/email_article_plugin_myprofile_controller.rb
@@ -5,6 +5,8 @@ class EmailArticlePluginMyprofileController < MyProfileController
 #  before_filter :store_location, :only => [:join, :join_not_logged, :report_abuse, :send_mail]
 #  before_filter :login_required, :only => [:add, :join, :join_not_logged, :leave, :unblock, :leave_scrap, :remove_scrap, :remove_activity, :view_more_activities, :view_more_network_activities, :report_abuse, :register_report, :leave_comment_on_activity, :send_mail]
  
+  
+  
   def send_email
 #    puts "ID ***************************"
 #    y params[:id]
@@ -12,11 +14,13 @@ class EmailArticlePluginMyprofileController < MyProfileController
 #    profile = Profile[params[:profile]]
     article = Article.find(params[:id])
     EmailArticlePluginMyprofileController::Sender.deliver_mail(article)
+    render :text=>'ok'
   end
   
   
   class Sender < ActionMailer::Base
     def mail(article)
+
       members = article.profile.members
       emails = []
       members.each{ |m|
