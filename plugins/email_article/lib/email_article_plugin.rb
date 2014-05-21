6a66ab8953dff6a665ad2fed46748eaf86ba66bb
Evandro Junior <evandrojr@gmail.com>
2014-05-21 17:45:29 +0000

    Improved messages

--- plugins/email_article/lib/email_article_plugin.rb
+++ plugins/email_article/lib/email_article_plugin.rb
@@ -12,17 +12,17 @@ class EmailArticlePlugin < Noosfero::Plugin
     label = _("Send article to members")
     htmlclass = _("button with-text icon-menu-mail")
     title = _("Email article to all community members")
-    
+
     lambda {
-      if  !profile.blank? and !user.blank? and user.is_admin?(profile) and @page.kind_of?(TextArticle) 
+      if  !profile.blank? and !user.blank? and user.is_admin?(profile) and @page.kind_of?(TextArticle)
         link_to_remote(
             label,
             {
                :url => { :controller => 'email_article_plugin_myprofile', :action => "send_email", :id => @page},
                :method => :get,
-               :success => "alert('Emails enviados')",
-               :failure => "alert('Erro ao enviar email')",
-               :confirm => _("Are you sure you want to email this article to the all community members?"),
+               :success => "alert('" + _("Messages sent to outbox, soon will be delivered") + "')",
+               :failure => "alert('" + _("Error sending emails") + "')",
+               :confirm => _("Are you sure you want to email this article to all community members?"),
             },
             :class => htmlclass,
             :title => title
