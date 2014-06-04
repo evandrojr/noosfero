3e1f0a87ca43417d248390803973f8e78fb0849d
Leandro Nunes dos Santos <leandronunes@gmail.com>
2014-06-04 12:51:33 -0300

    removing default value for serpro_integration_plugin variable

--- plugins/serpro_integration/lib/ext/community.rb
+++ plugins/serpro_integration/lib/ext/community.rb
@@ -1,5 +1,5 @@
 require_dependency 'community'
-require 'gitlab'
+#require 'gitlab'
 #require 'jenkins_api_client'
 
 class Community
@@ -9,7 +9,7 @@ class Community
   settings_items :allow_jenkins_integration, :type => :boolean, :default => true
 
   #FIXME make test for default option
-  settings_items :serpro_integration_plugin, :type => Hash, :default => {}
+  settings_items :serpro_integration_plugin, :type => Hash
 
   ##########################################
   #             Gitlab stuff               #
@@ -22,6 +22,7 @@ class Community
   end
 
   def gitlab
+    self.serpro_integration_plugin ||= {}
     self.serpro_integration_plugin[:gitlab] ||= {}
     self.serpro_integration_plugin[:gitlab]
   end
@@ -129,84 +130,86 @@ class Community
                                      :password => $jenkins_private_token,
                                      :username => $jenkins_user)
 
-    xmlJenkins = "
-        <maven2-moduleset plugin='maven-plugin@1.509'>
-            <actions/>
-            <description>Projeto criado para o repositório #{repositoryPath} do Gitlab - #{webUrl}</description>
-            <logRotator class='hudson.tasks.LogRotator'>
-                <daysToKeep>-1</daysToKeep>
-                <numToKeep>2</numToKeep>
-                <artifactDaysToKeep>-1</artifactDaysToKeep>
-                <artifactNumToKeep>-1</artifactNumToKeep>
-            </logRotator>
-            <keepDependencies>false</keepDependencies>
-            <properties/>
-            <scm class='hudson.plugins.git.GitSCM' plugin='git@2.2.1'>
-                <configVersion>2</configVersion>
-                <userRemoteConfigs>
-                    <hudson.plugins.git.UserRemoteConfig>
-                        <url>#{gitUrl}</url>
-                    </hudson.plugins.git.UserRemoteConfig>
-                </userRemoteConfigs>
-                <branches>
-                    <hudson.plugins.git.BranchSpec>
-                    <name>*/master</name>
-                    </hudson.plugins.git.BranchSpec>
-                </branches>
-                <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
-                <submoduleCfg class='list'/>
-                <extensions/>
-            </scm>
-            <canRoam>true</canRoam>
-            <disabled>false</disabled>
-            <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
-            <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
-            <jdk>(Inherit From Job)</jdk>
-            <triggers class='vector'/>
-            <concurrentBuild>false</concurrentBuild>
-            <goals>clean package install deploy</goals>
-            <aggregatorStyleBuild>true</aggregatorStyleBuild>
-            <incrementalBuild>false</incrementalBuild>
-            <perModuleEmail>true</perModuleEmail>
-            <ignoreUpstremChanges>false</ignoreUpstremChanges>
-            <archivingDisabled>false</archivingDisabled>
-            <resolveDependencies>false</resolveDependencies>
-            <processPlugins>false</processPlugins>
-            <mavenValidationLevel>-1</mavenValidationLevel>
-            <runHeadless>false</runHeadless>
-            <disableTriggerDownstreamProjects>false</disableTriggerDownstreamProjects>
-            <settings class='jenkins.mvn.DefaultSettingsProvider'/>
-            <globalSettings class='jenkins.mvn.DefaultGlobalSettingsProvider'/>
-            <reporters>
-                <hudson.maven.reporters.MavenMailer>
-                    <recipients/>
-                    <dontNotifyEveryUnstableBuild>false</dontNotifyEveryUnstableBuild>
-                    <sendToIndividuals>true</sendToIndividuals>
-                    <perModuleEmail>true</perModuleEmail>
-                </hudson.maven.reporters.MavenMailer>
-            </reporters>
-            <publishers>
-                <hudson.plugins.sonar.SonarPublisher plugin='sonar@2.1'>
-                    <jdk>(Inherit From Job)</jdk>
-                    <branch/>
-                    <language/>
-                    <mavenOpts/>
-                    <jobAdditionalProperties/>
-                    <settings class='jenkins.mvn.DefaultSettingsProvider'/>
-                    <globalSettings class='jenkins.mvn.DefaultGlobalSettingsProvider'/>
-                    <usePrivateRepository>false</usePrivateRepository>
-                </hudson.plugins.sonar.SonarPublisher>
-            </publishers>
-            <buildWrappers/>
-            <prebuilders/>
-            <postbuilders/>
-            <runPostStepsIfResult>
-                <name>FAILURE</name>
-                <ordinal>2</ordinal>
-                <color>RED</color>
-            </runPostStepsIfResult>
-        </maven2-moduleset>
-    "
+    xmlJenkins = ""
+
+#    xmlJenkins = "
+#        <maven2-moduleset plugin='maven-plugin@1.509'>
+#            <actions/>
+#            <description>Projeto criado para o repositório #{repositoryPath} do Gitlab - #{webUrl}</description>
+#            <logRotator class='hudson.tasks.LogRotator'>
+#                <daysToKeep>-1</daysToKeep>
+#                <numToKeep>2</numToKeep>
+#                <artifactDaysToKeep>-1</artifactDaysToKeep>
+#                <artifactNumToKeep>-1</artifactNumToKeep>
+#            </logRotator>
+#            <keepDependencies>false</keepDependencies>
+#            <properties/>
+#            <scm class='hudson.plugins.git.GitSCM' plugin='git@2.2.1'>
+#                <configVersion>2</configVersion>
+#                <userRemoteConfigs>
+#                    <hudson.plugins.git.UserRemoteConfig>
+#                        <url>#{gitUrl}</url>
+#                    </hudson.plugins.git.UserRemoteConfig>
+#                </userRemoteConfigs>
+#                <branches>
+#                    <hudson.plugins.git.BranchSpec>
+#                    <name>*/master</name>
+#                    </hudson.plugins.git.BranchSpec>
+#                </branches>
+#                <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+#                <submoduleCfg class='list'/>
+#                <extensions/>
+#            </scm>
+#            <canRoam>true</canRoam>
+#            <disabled>false</disabled>
+#            <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+#            <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+#            <jdk>(Inherit From Job)</jdk>
+#            <triggers class='vector'/>
+#            <concurrentBuild>false</concurrentBuild>
+#            <goals>clean package install deploy</goals>
+#            <aggregatorStyleBuild>true</aggregatorStyleBuild>
+#            <incrementalBuild>false</incrementalBuild>
+#            <perModuleEmail>true</perModuleEmail>
+#            <ignoreUpstremChanges>false</ignoreUpstremChanges>
+#            <archivingDisabled>false</archivingDisabled>
+#            <resolveDependencies>false</resolveDependencies>
+#            <processPlugins>false</processPlugins>
+#            <mavenValidationLevel>-1</mavenValidationLevel>
+#            <runHeadless>false</runHeadless>
+#            <disableTriggerDownstreamProjects>false</disableTriggerDownstreamProjects>
+#            <settings class='jenkins.mvn.DefaultSettingsProvider'/>
+#            <globalSettings class='jenkins.mvn.DefaultGlobalSettingsProvider'/>
+#            <reporters>
+#                <hudson.maven.reporters.MavenMailer>
+#                    <recipients/>
+#                    <dontNotifyEveryUnstableBuild>false</dontNotifyEveryUnstableBuild>
+#                    <sendToIndividuals>true</sendToIndividuals>
+#                    <perModuleEmail>true</perModuleEmail>
+#                </hudson.maven.reporters.MavenMailer>
+#            </reporters>
+#            <publishers>
+#                <hudson.plugins.sonar.SonarPublisher plugin='sonar@2.1'>
+#                    <jdk>(Inherit From Job)</jdk>
+#                    <branch/>
+#                    <language/>
+#                    <mavenOpts/>
+#                    <jobAdditionalProperties/>
+#                    <settings class='jenkins.mvn.DefaultSettingsProvider'/>
+#                    <globalSettings class='jenkins.mvn.DefaultGlobalSettingsProvider'/>
+#                    <usePrivateRepository>false</usePrivateRepository>
+#                </hudson.plugins.sonar.SonarPublisher>
+#            </publishers>
+#            <buildWrappers/>
+#            <prebuilders/>
+#            <postbuilders/>
+#            <runPostStepsIfResult>
+#                <name>FAILURE</name>
+#                <ordinal>2</ordinal>
+#                <color>RED</color>
+#            </runPostStepsIfResult>
+#        </maven2-moduleset>
+#    "
 
     begin
         @client.job.create(projectName, xmlJenkins)
