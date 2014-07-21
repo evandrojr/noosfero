f6769a0f12b7ec1ae6e32185bfc2224ff77b539f
Evandro Jr <evandrojr@gmail.com>
2014-07-21 12:09:55 -0300

    toggle comment and view article is now working

--- plugins/comment_paragraph/public/comment_paragraph.js
+++ plugins/comment_paragraph/public/comment_paragraph.js
@@ -1,7 +1,20 @@
 function makeCommentable() {
   paragraphsTxt=""
+
+  foundCommentableParagraph = false;
+  jQuery('#article_body_ifr').contents().find('body').children('div.article_comments').each(function( index ) {
+        paragraphsTxt+="<p>" + jQuery(this).html() + "</p>";
+      console.log(jQuery(this).html())
+      foundCommentableParagraph = true;
+  });
+
+  if(foundCommentableParagraph === true){
+    tinyMCE.activeEditor.setContent(paragraphsTxt)
+    return;
+  }
+  
   jQuery('#article_body_ifr').contents().find('body').children('p').each(function( index ) {
-      paragraphsTxt+='<p><div class="macro article_comments" data-macro="comment_paragraph_plugin/allow_comment" data-macro-paragraph_id="' + index + '">' + jQuery(this).html() + '</p></div>'
+      paragraphsTxt+='<p><div class="macro article_comments" data-macro="comment_paragraph_plugin/allow_comment" data-macro-paragraph_id="' + index + '">' + jQuery(this).html() + '</div></p><br>'
   });
   tinyMCE.activeEditor.setContent(paragraphsTxt)
 }
