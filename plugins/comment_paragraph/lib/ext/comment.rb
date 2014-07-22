9e8fcf2d2468c10ae595865f9f7b136f5c80c138
Evandro Jr <evandrojr@gmail.com>
2014-07-22 11:29:33 -0300

    working with textarea

--- plugins/comment_paragraph/lib/ext/comment.rb
+++ plugins/comment_paragraph/lib/ext/comment.rb
@@ -3,12 +3,14 @@ require_dependency 'comment'
 class Comment
 
   scope :without_paragraph, :conditions => {:paragraph_id => nil }
-
+  
+  settings_items :comment_paragraph_selected_area, :type => :string
+ 
   scope :in_paragraph, proc { |paragraph_id| {
       :conditions => ['paragraph_id = ?', paragraph_id]
     }
   }
 
-  attr_accessible :paragraph_id
+  attr_accessible :paragraph_id, :comment_paragraph_selected_area
 
 end
