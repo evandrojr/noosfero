df303518ef8e36ba7d4e0a736a880cc8c01adbbb
Evandro Jr <evandrojr@gmail.com>
2014-07-31 10:03:18 -0300

    Validation and comment from email fix

--- plugins/comment_paragraph/lib/ext/article.rb
+++ plugins/comment_paragraph/lib/ext/article.rb
@@ -8,7 +8,7 @@ class Article
 
   def body_change_with_comments
     if body && body_changed? && !self.comments.empty?
-      paragraphs_with_comments = self.comments.where("'article_id' IS NOT NULL")
+      paragraphs_with_comments = self.comments.where("'paragraph_id' IS NOT NULL")
       errors[:base] << (N_('You are unable to change the body of the article when paragraphs are commented')) unless (paragraphs_with_comments).empty?
     end
   end
--- plugins/comment_paragraph/public/comment_paragraph_macro.js
+++ plugins/comment_paragraph/public/comment_paragraph_macro.js
@@ -64,13 +64,13 @@ jQuery(document).ready(function($) {
 //    cssApplier.toggleSelection();
 //  });
 
-  function processSomething(){
+  function processAnchor(){
     var anchor = window.location.hash;
     if(anchor.length==0) return;
 
     var val = anchor.split('-'); //anchor format = #comment-\d+
     if(val.length!=2 || val[0]!='#comment') return;
-    if($('div[data-macro=comment_paragraph_plugin/allow_comment]').length==0) return; //comment_paragraph_plugin/allow_comment div must exists
+    if($('div[data-macro=comment_paragraph_plugin\\/allow_comment]').length==0) return; //comment_paragraph_plugin/allow_comment div must exists
     var comment_id = val[1];
     if(!/^\d+$/.test(comment_id)) return; //test for integer
 
@@ -85,7 +85,7 @@ jQuery(document).ready(function($) {
     });
   }
   
-  processSomething();
+  processAnchor();
   
   $(document).on('mouseover', 'li.article-comment', function(){
     var selected_area = $(this).find('input.paragraph_comment_area').val();
