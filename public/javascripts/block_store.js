4c5ccf60daf43cf5481a010c94ff328aa77e8ca8
Francisco Marcelo de Araújo Lima Júnior <francisco.lima-junior@serpro.gov.br>
2014-08-12 17:40:19 -0300

    add effects for scrolling blocks

--- public/javascripts/block_store.js
+++ public/javascripts/block_store.js
@@ -1,37 +1,25 @@
 jQuery(document).ready(function () {
-
-  // navigation - click action
   jQuery('#block-types-navigation a.previous').click(PreviousSlide);
   jQuery('#block-types-navigation a.next').click(NextSlide);
-
-  // set first block types group active
   jQuery('.block-types-group').first().addClass('active');
   jQuery(".block-types-group.active + .block-types-group").addClass('next');
-
   firstBlockTypesGroup = jQuery(".block-types-group").first();
   lastBlockTypesGroup = jQuery(".block-types-group").last();
+  jQuery('.block-types-group').first().show( 'slide', { direction : 'down' }, 1000 );
   jQuery("#block-types-navigation a.previous").hide();
 });
 
 function NextSlide() {
-    // get active block-types-group
     activeBlockTypesGroup = jQuery(".block-types-group.active");
-
-    // get next block-types-group
     nextBlockTypesGroup = jQuery(".block-types-group.next").first();
-
-    // switch active group
     activeBlockTypesGroup.removeClass('active');
     activeBlockTypesGroup.addClass('prev');
-    activeBlockTypesGroup.css({"display":"none"});
+    activeBlockTypesGroup.hide( 'slide', { direction : 'left' }, 1000 );
     nextBlockTypesGroup.removeClass('next');
     nextBlockTypesGroup.addClass('active');
-    nextBlockTypesGroup.css({"display": "block"});
-
     jQuery(".block-types-group.active + .block-types-group").addClass('next');
-
+    nextBlockTypesGroup.show( 'slide', { direction : 'right' }, 1000 );
     activeBlockTypesGroup = jQuery(".block-types-group.active");
-
     jQuery("#block-types-navigation a.previous").show();
 
     if ( activeBlockTypesGroup.is( lastBlockTypesGroup ) ) {
@@ -40,26 +28,18 @@ function NextSlide() {
 }
 
 function PreviousSlide() {
-    // get active block-types-group
     activeBlockTypesGroup = jQuery(".block-types-group.active");
-
-    // get previous block-types-group
     previousBlockTypesGroup = jQuery(".block-types-group.prev").last();
-
-    // switch active group
     activeBlockTypesGroup.removeClass('active');
     activeBlockTypesGroup.addClass('next');
-    activeBlockTypesGroup.css({"display":"none"});
+    activeBlockTypesGroup.hide( 'slide', { direction : 'right' }, 1000 );
     previousBlockTypesGroup.removeClass('prev');
     previousBlockTypesGroup.addClass('active');
-    previousBlockTypesGroup.css({"display": "block"});
-
+    previousBlockTypesGroup.show( 'slide', { direction : 'left' }, 1000 );
     activeBlockTypesGroup = jQuery(".block-types-group.active");
-
     jQuery("#block-types-navigation a.next").show();
 
     if ( activeBlockTypesGroup.is( firstBlockTypesGroup ) ) {
       jQuery("#block-types-navigation a.previous").hide();
     }
 }
-
