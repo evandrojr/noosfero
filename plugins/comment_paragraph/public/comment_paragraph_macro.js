6675af8444339d160c9fb2b0d72ba417a4ee1782
Evandro Jr <evandrojr@gmail.com>
2014-09-01 17:07:17 -0300

    major changes in progress

--- plugins/comment_paragraph/public/comment_paragraph_macro.js
+++ plugins/comment_paragraph/public/comment_paragraph_macro.js
@@ -10,93 +10,48 @@ function getIdCommentParagraph(paragraphId){
   return paragraphId.substring(idx+1, paragraphId.length);
 }
 
-function teleportToBody(element){
-    var $ = jQuery;
-    element.detach();
-    $('body').append(element);
-}
-
-function moveCommentsToTheSide(paragraphId, mouseX, mouseY){
-  var $ = jQuery;
-  //var element = sideCommentForm;
-  //element.detach();
-  //if ( $('body #side_comment_form').size === 0){
-//    $('body').append(element);
-//  }
-  //element.css({top: 0, border-right: 20, position:'absolute'});
-}
-
-//function buildSideCommentBlock(){
-//  var $ = jQuery;
-//  $("body").append('\
-//    <div align="left" id="side_comments_box" class="side-comments-box">\n\
-//      [side comments collumn]<BR>\n\
-//      [side comments collumn]<BR>\n\
-//    </div>\n\
-//  ');
-//  //Detects if there is any comment paragraph
-//  
-//  //  var button = jQuery('#page-comment-form-' + paragraphId +  ' a')[0];
-//}
-
-function buildSideCommentBox(id, commentCount ){
-  var $ = jQuery;
-  //Search to top of the respective paragraph
-  var top = $('#comment_paragraph_' + id).offset().top;
-  var right = $('#comment_paragraph_' + id).offset().left + $('#comment_paragraph_' + id).width() + 20;
-  
-  var boxComments="\n\
-    <div class='side-comments-counter'\n\
-        id='side_comment_counter_" + id + "' style='top: " + top + "px; left: " + (right - 20) + "px;'  >+</div> \n\
-    </div>";
-    //<div class='side-comments-box'  id='side_comment_box_" + id + "' style='top: " + top + "px; left: " + right + "px;' ></div>"; 
-  return boxComments;
-}
-
-function putSideComments(){
-  var $ = jQuery;
-
-  $(".comment-count").each(function(index){
-    //Include all comments except the last
-    var idElement = $(this).attr('id');
-    if(idElement != undefined ){
-      var n = idElement.lastIndexOf('-');
-      var id_number =  idElement.substr(n + 1, idElement.length - n +1);
-      firstTimeOpenParagraph[id_number] = true;
-      commentParagraphIds.push(id_number);
-      var commentCount = $(this).text().trim();
-      commentCount = parseInt(commentCount);
-      var box = buildSideCommentBox(id_number, commentCount);
-      $('body').append(box);
-      if(commentCount > 0){
-        $('#side_comment_counter_' + id_number).text(commentCount);
-//        $('#side_comment_counter_' + id_number).show();
-      }
-      $('#side_comment_counter_' + id_number).show();
-    }
-  });      
-}
-
-
-
 jQuery(document).ready(function($) {
+  $('.display-comment-form').unbind();
+  $('.display-comment-form').click(function(){
+    var $button = $(this);
+    showBox($button.parents('.post_comment_box'));
+    $($button).hide();
+    $button.closest('.page-comment-form').find('input').first().focus();
+    return false;
+  });
+
+  $('#cancel-comment').die();
+  $('#cancel-comment').live("click", function(){
+    var $button = $(this);
+    showBox($button.parents('.post_comment_box'));
+    show_display_comment_button();
+    var page_comment_form = $button.parents('.page-comment-form');
+    page_comment_form.find('.errorExplanation').remove();
+    $(this).closest("textarea[name^='comment'").text("");
+    $(this).closest("div[class^='side-comment']").hide(); 
+    return false;
+  });
+
+  function showBox(div){
+    if(div.hasClass('closed')) {
+      div.removeClass('closed');
+      div.addClass('opened');
+    } 
+  }  
+  
+  
   //Hides old style ballons
   $("img[alt|=Comments]").hide();
-  //Hides old style counter
-  $(".comment-count").hide();
-  
   rangy.init();
   cssApplier = rangy.createCssClassApplier("commented-area", {normalize: false});
-  
   //Add marked text bubble
   $("body").append('\
-      <a href="#" id="comment-bubble" style="width:120px;text-decoration: none">\
+      <a id="comment-bubble" style="width:120px;text-decoration: none">\
           <div align="center"  class="triangle-right" >Comentar<br>+</div>\
       </a>');
-  //buildSideCommentBlock();
-  //
+
   //Creates a side bubble for each paragraph with the amount of comments
-  putSideComments();
+  //putSideComments();
 
   $('.comment_paragraph').mouseover(function(){
     var paragraphId = getIdCommentParagraph($(this)[0].id);
@@ -112,43 +67,61 @@ jQuery(document).ready(function($) {
   
   $('.side-comments-counter').click(function(){
     hideAllComments();
-    var paragraphId = getIdCommentParagraph($(this).attr('id'));
-    $('#side_comment_box_' + paragraphId).toggle();  
+    var paragraphId = $(this).data('paragraph')
+    $('#side_comment_' + paragraphId).show();
+    $('#comments_list_toggle_paragraph_' + paragraphId).show();
+    console.log(paragraphId);
     //Loads the comments
     var url = $('#link_to_ajax_comments_' + paragraphId).data('url'); 
-    $('.comments_list_toggle_paragraph_' + paragraphId).show();
-    if(firstTimeOpenParagraph[paragraphId]===true ){
-      firstTimeOpenParagraph[paragraphId]=false;
-      $.ajax({
-        dataType: "script",
-        url: url
-      }).done(function() {
-        var button = $('#page-comment-form-' + paragraphId + ' a').get(0);
-        button.click();
-        //teleportToBody($('.comments_list_toggle_paragraph_' + paragraphId));
-        var top = $('#comment_paragraph_' + paragraphId).offset().top;
-        var right = $('#comment_paragraph_' + paragraphId).offset().left + $('#comment_paragraph_' + paragraphId).width();
-        top += -120;
-        right+= -500;
-        $('.comments_list_toggle_paragraph_' + paragraphId).css('position','absolute');
-        $('.comments_list_toggle_paragraph_' + paragraphId).css('top',top);
-        $('.comments_list_toggle_paragraph_' + paragraphId).css('left',right);
-        $('.comments_list_toggle_paragraph_' + paragraphId).css('background','#FFFFFF');
-        $('label[for|=comment_title]').hide();
-        $('label[for|=comment_body]').css({top: -30, left: +20, position:'relative'});
-        $('.comment_form p').hide();
-        $('.comments_list_toggle_paragraph_' + paragraphId).width('250px');
-      });
-    }
+    firstTimeOpenParagraph[paragraphId]=false;
+    $.ajax({
+      dataType: "script",
+      url: url
+    }).done(function() {
+      var button = $('#page-comment-form-' + paragraphId + ' a').get(0);
+      button.click();
+      alignSideComments(paragraphId);
+    });
   });
-
+  
+  
+  $('#comment-bubble').click(function(event){
+    this.hide();
+    hideAllComments();
+    $("#comment-bubble").css({top: 0, left: 0, position:'absolute'});
+    var url = $("#comment-bubble").data('url');
+    var paragraphId = $("#comment-bubble").data("paragraphId");
+    $('#side_comment_' + paragraphId).show();
+    $.ajax({
+      dataType: "script",
+      url: url
+    }).done(function() {
+      var button = $('#page-comment-form-' + paragraphId + ' a').get(0);
+      button.click();
+      alignSideComments(paragraphId);  
+    });
+  });  
+   
+  function alignSideComments(paragraphId){
+//    var top = $('#comment_paragraph_' + paragraphId).offset().top;
+//    var right = $('#comment_paragraph_' + paragraphId).offset().left + $('#comment_paragraph_' + paragraphId).width();
+//    top += -120;
+//    right+= -500;
+//    $('.comments_list_toggle_paragraph_' + paragraphId).css('position','absolute');
+//    $('.comments_list_toggle_paragraph_' + paragraphId).css('display','inline');
+//    $('.comments_list_toggle_paragraph_' + paragraphId).css('top',top);
+//    $('.comments_list_toggle_paragraph_' + paragraphId).css('left',right);
+    $('.comments_list_toggle_paragraph_' + paragraphId).css('background','#FFFFFF');
+    $('label[for|=comment_title]').hide();
+    $('label[for|=comment_body]').css({top: -30, left: +20, position:'relative'});
+    $('.comment_form p').hide();
+    $('.comments_list_toggle_paragraph_' + paragraphId).width('250px');
+    $('.required-field').removeClass("required-field");
+  } 
    
   function hideAllComments(){
-    for (index = 0; index < commentParagraphIds.length; ++index) {
-      $('.comments_list_toggle_paragraph_' + commentParagraphIds[index]).hide();
-    }
+    $(".side-comment").hide();
   } 
-
   
   $("#comment-bubble").hide();
   //Undo previous highlight from the paragraph
@@ -162,13 +135,7 @@ jQuery(document).ready(function($) {
       rootElement.innerHTML = lastParagraph[paragraphId];
     }
   });
-  
-  
-//  $('#comment-bubble').mouseleave(function(){
-//    this.hide();
-//    $("#comment-bubble").css({top: 0, left: 0, position:'absolute'});
-//  });
- 
+
    //highlight area from the paragraph
   $('.comment_paragraph').mouseup(function(event){
     var paragraphId = getIdCommentParagraph($(this)[0].id);
@@ -200,31 +167,9 @@ jQuery(document).ready(function($) {
     rootElement.focus();
   });
   
- 
-//  $('#comment-bubble').click(function(event){
-//    this.hide();
-//    hideAllComments();
-//    $("#comment-bubble").css({top: 0, left: 0, position:'absolute'});
-//    var url = $("#comment-bubble").data('url');
-//    var paragraphId = $("#comment-bubble").data("paragraphId");
-//
-//    $('.comments_list_toggle_paragraph_' + paragraphId).show();
-//    $.ajax({
-//      dataType: "script",
-//      url: url
-//    }).done(function() {
-//      var button = $('#page-comment-form-' + paragraphId + ' a').get(0)
-//      button.click();
-//      window.location="#page-comment-form-" + paragraphId;
-////      //Move comments
-////      sideCommentForm = $('#side_comment_form');
-////      sideCommentForm.hide();
-////      sideCommentForm = $('.comment_form').first();
-////      sideCommentForm.attr("id",'side_comment_form');
-////      moveCommentsToTheSide(paragraphId, event.pageX, event.pageY);
-//    });
-//  });
-  
+  $('.article-body').mousedown(function(event){
+    $( ".commented-area" ).contents().unwrap();
+  });
 
   function processAnchor(){
     var anchor = window.location.hash;
@@ -244,19 +189,15 @@ jQuery(document).ready(function($) {
     }).done(function() {
       var button = $('#page-comment-form-' + comment_id + ' a').get(0)
       button.click();
-////        window.location="#page-comment-form-" + paragraphId;
-//      //Move comments
-//      sideCommentForm = $('#side_comment_form');
-//      sideCommentForm.hide();
-//      sideCommentForm = $('.comment_form').first();
-//      sideCommentForm.attr("id",'side_comment_form');
-//      moveCommentsToTheSide(paragraphId, event.pageX, event.pageY);
+      //alignSideComments(paragraphId); 
     });
   }
  
   processAnchor();
  
   $(document).on('mouseover', 'li.article-comment', function(){
+    console.log("mouseover");
+    
     var selected_area = $(this).find('input.paragraph_comment_area').val();
     var paragraph_id =  $(this).find('input.paragraph_id').val();
     var rootElement = $('#comment_paragraph_'+ paragraph_id).get(0);
@@ -274,6 +215,7 @@ jQuery(document).ready(function($) {
   });
  
   $(document).on('mouseout', 'li.article-comment', function(){
+    console.log("mouseout");
     var paragraph_id =  $(this).find('input.paragraph_id').val();
     var rootElement = $('#comment_paragraph_'+ paragraph_id).get(0);
   
@@ -288,7 +230,13 @@ jQuery(document).ready(function($) {
       sel.removeAllRanges();
     }
   });
-});
+  
+//  $('.icon-cancel').unbind('click').click(function(e){
+//    e.preventDefault();
+//    $(this).closest("textarea[name^='comment'").text("");
+//    $(this).closest("div[class^='comments_list_toggle_paragraph_']").hide(); 
+//  });
+}); // End of jQuery(document).ready(function($)
 
 function toggleParagraph(paragraph) {
   var div = jQuery('div.comments_list_toggle_paragraph_'+paragraph);
