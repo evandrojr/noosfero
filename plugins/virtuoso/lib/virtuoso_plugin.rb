2b3f66cd64138bce0166a7d88f22b907e4b70d39
Evandro Jr <evandrojr@gmail.com>
2014-11-13 11:06:07 -0300

    added js file to enable multiple servers

--- plugins/virtuoso/lib/virtuoso_plugin.rb
+++ plugins/virtuoso/lib/virtuoso_plugin.rb
@@ -19,5 +19,9 @@ class VirtuosoPlugin < Noosfero::Plugin
   def virtuoso_client
     @virtuoso_client ||= RDF::Virtuoso::Repository.new("#{settings.virtuoso_uri}/sparql", :update_uri => "#{settings.virtuoso_uri}/sparql-auth", :username => settings.virtuoso_username, :password => settings.virtuoso_password, :auth_method => 'digest', :timeout => 30)
   end
+  
+  def js_files
+    ['edit-server-list']
+  end  
 
 end
