4a8f0c532b71e84c6d654f11c8e0ed81a081fed2
Evandro Jr <evandrojr@gmail.com>
2014-11-21 14:30:45 -0300

    Deleted old plugin files

--- plugins/video/test/unit/video_plugin_test.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-class VideoPluginTest < ActiveSupport::TestCase
-
-  should "return VideoBlock in extra_blocks class method" do
-    assert  VideoPlugin.extra_blocks.keys.include?(VideoPlugin::VideoBlock)
-  end
-
-end
