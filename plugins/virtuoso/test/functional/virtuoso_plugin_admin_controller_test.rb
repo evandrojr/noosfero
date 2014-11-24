cdca25dccb5c9ab31e106283164e06411768327b
Evandro Jr <evandrojr@gmail.com>
2014-11-24 19:57:21 -0300

    Added last harvest for each dspace server

--- plugins/virtuoso/test/functional/virtuoso_plugin_admin_controller_test.rb
+++ plugins/virtuoso/test/functional/virtuoso_plugin_admin_controller_test.rb
@@ -17,9 +17,11 @@ class VirtuosoPluginAdminControllerTest < ActionController::TestCase
       :virtuoso_readonly_username=>"readonly_username",
       :virtuoso_readonly_password=>"readonly_password",
       :dspace_servers=>[
-        {"dspace_uri"=>"http://dspace1.noosfero.com"},
+        {"dspace_uri"=>"http://dspace1.noosfero.com","last_harvest" => 5 },
         {"dspace_uri"=>"http://dspace2.noosfero.com"},
-        {"dspace_uri"=>"http://dspace3.noosfero.com"}
+        {"dspace_uri"=>"http://dspace3.noosfero.com", "last_harvest" => 0},
+        {"dspace_uri"=>"http://dspace4.noosfero.com", "last_harvest" => nil},
+        {"dspace_uri"=>"http://dspace5.noosfero.com", "last_harvest" => 9},
       ]
     }
   end
@@ -35,6 +37,7 @@ class VirtuosoPluginAdminControllerTest < ActionController::TestCase
     assert_equal 'http://dspace1.noosfero.com', @settings.settings[:dspace_servers][0][:dspace_uri]
     assert_equal 'http://dspace2.noosfero.com', @settings.settings[:dspace_servers][1][:dspace_uri]
     assert_equal 'http://dspace3.noosfero.com', @settings.settings[:dspace_servers][2][:dspace_uri]
+    assert_equal  "9", @settings.settings[:dspace_servers][4][:last_harvest]
     assert_redirected_to :action => 'index'
   end
 
@@ -45,7 +48,7 @@ class VirtuosoPluginAdminControllerTest < ActionController::TestCase
 
   should 'create delayed job to start harvest on force action' do
     post :index, :settings => mock_settings
-    harvest = VirtuosoPlugin::DspaceHarvest.new(environment, "http://dspace1.noosfero.com")
+    harvest = VirtuosoPlugin::DspaceHarvest.new(environment, {"dspace_uri"=>"http://dspace1.noosfero.com", "last_harvest" => 5 })
     assert !harvest.find_job.present?
     get :force_harvest
     assert harvest.find_job.present?
@@ -54,17 +57,17 @@ class VirtuosoPluginAdminControllerTest < ActionController::TestCase
   should 'force harvest from start' do
     post :index, :settings => mock_settings
     get :force_harvest, :from_start => true
-    harvest = VirtuosoPlugin::DspaceHarvest.new(environment, "http://dspace2.noosfero.com")
+    harvest = VirtuosoPlugin::DspaceHarvest.new(environment, {"dspace_uri"=>"http://dspace4.noosfero.com", "last_harvest" => nil})
     assert harvest.find_job.present?
     assert_equal nil, harvest.settings.last_harvest
   end
- 
+
   should 'not create delayed job to start harvest on force action without settings' do
     post :index, :settings => mock_settings
-    harvest = VirtuosoPlugin::DspaceHarvest.new(environment, "http://dspace8.noosfero.com")
+    harvest = VirtuosoPlugin::DspaceHarvest.new(environment)
     assert !harvest.find_job.present?, "testing if no job is running"
     get :force_harvest
-    assert !harvest.find_job.present?, "testing if no job is running again" 
+    assert !harvest.find_job.present?, "testing if no job is running again"
   end
- 
+
 end
