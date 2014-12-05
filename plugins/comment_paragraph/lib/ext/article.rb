0eb620f08016c7da4f2c7c2253e4977b2cc9502b
Evandro Jr <evandrojr@gmail.com>
2014-12-05 18:12:50 -0300

    validation for body_change_with_comments removed

--- plugins/comment_paragraph/lib/ext/article.rb
+++ plugins/comment_paragraph/lib/ext/article.rb
@@ -4,7 +4,7 @@ class Article
 
   has_many :paragraph_comments, :class_name => 'Comment', :foreign_key => 'source_id', :dependent => :destroy, :order => 'created_at asc', :conditions => [ 'paragraph_id IS NOT NULL']
 
-  validate :body_change_with_comments
+  #validate :body_change_with_comments
 
   def body_change_with_comments
     if body && body_changed? && !self.comments.empty?
