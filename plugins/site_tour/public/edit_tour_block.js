d75dc8aeea45cb8603cde9b08456d32d782354ce
Victor Costa <vfcosta@gmail.com>
2015-01-20 11:38:24 -0300

    site_tour: set triggers for groups

--- plugins/site_tour/public/edit_tour_block.js
+++ plugins/site_tour/public/edit_tour_block.js
@@ -1,16 +1,17 @@
-function add_new_action() {
-  var new_action = jQuery('#edit-tour-block #new-template>li').clone();
-  new_action.show();
-  jQuery('#droppable-tour-actions').append(new_action);
-}
-
 jQuery(document).ready(function(){
-  jQuery('#edit-tour-block').on('click', '.delete-tour-action-row', function() {
+  jQuery('#edit-tour-block').on('click', '.add-item', function() {
+    var container = jQuery(this).closest('.list-items');
+    var new_action = container.find('#new-template>li').clone();
+    new_action.show();
+    container.find('.droppable-items').append(new_action);
+  });
+
+  jQuery('#edit-tour-block').on('click', '.delete-tour-block-item', function() {
     jQuery(this).parent().parent().remove();
     return false;
   });
 
-  jQuery("#droppable-tour-actions").sortable({
+  jQuery("#edit-tour-block .droppable-items").sortable({
     revert: true,
     axis: "y"
   });
