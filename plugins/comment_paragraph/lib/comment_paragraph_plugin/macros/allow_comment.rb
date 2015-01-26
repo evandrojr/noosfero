65df7c9a213231bf52de59540ed3f61929d698a1
Victor Costa <vfcosta@gmail.com>
2015-01-26 12:28:15 -0300

    comment_paragraph: remove macro button

--- plugins/comment_paragraph/lib/comment_paragraph_plugin/macros/allow_comment.rb
+++ plugins/comment_paragraph/lib/comment_paragraph_plugin/macros/allow_comment.rb
@@ -1,14 +1,9 @@
-#FIXME See a better way to generalize this parameter.
 ActionView::Base.sanitized_allowed_attributes += ['data-macro', 'data-macro-paragraph_uuid']
 
 class CommentParagraphPlugin::AllowComment < Noosfero::Plugin::Macro
+
   def self.configuration
-    { :params => [],
-      :skip_dialog => true,
-      :generator => 'makeAllCommentable();',
-      :js_files => 'comment_paragraph.js',
-      :icon_path => '/plugins/comment_paragraph/images/balloons-comment.png',
-      :css_files => 'comment_paragraph.css' }
+    { :params => [] }
   end
 
   def parse(params, inner_html, source)
