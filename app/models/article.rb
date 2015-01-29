230eab09464cb5c4dcff9740ef888106de778220
Victor Costa <vfcosta@gmail.com>
2015-01-29 09:11:45 -0300

    Fix merge with master

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,7 @@ class Article < ActiveRecord::Base
                   :accept_comments, :feed, :published, :source,
                   :highlighted, :notify_comments, :display_hits, :slug,
                   :external_feed_builder, :display_versions, :external_link,
-                  :image_builder, :show_to_followers
+                  :image_builder, :show_to_followers, :published_at
 
   acts_as_having_image
 
