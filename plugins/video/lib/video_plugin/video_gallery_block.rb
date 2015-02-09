9b638609c960a3b38e52fdc66e95ca58617353d5
Evandro Junior <evandrojr@gmail.com>
2015-02-09 16:51:47 -0300

    removed trailing whitespaces

--- plugins/video/lib/video_plugin/video_gallery_block.rb
+++ plugins/video/lib/video_plugin/video_gallery_block.rb
@@ -1,11 +1,11 @@
 class VideoPlugin::VideoGalleryBlock < Block
 
   settings_items :video_gallery_id, :type => :integer
-  attr_accessible :video_gallery_id  
-  
+  attr_accessible :video_gallery_id
+
   include ActionView::Helpers
   include Rails.application.routes.url_helpers
-  
+
   def self.description
     _('Display a Video Gallery')
   end
@@ -20,12 +20,12 @@ class VideoPlugin::VideoGalleryBlock < Block
       video_gallery = VideoPlugin::VideoGallery.find(video_gallery_id)
       proc do
         render :partial => 'content_viewer/video_plugin/video_gallery', :locals => {:video_gallery => video_gallery}
-      end    
+      end
     end
   end
 
   def list_my_galleries
     Article.owner_video_galleries(owner)
   end
-  
+
 end
--- plugins/video/public/javascripts/videojs/vjs.youtube.js
+++ plugins/video/public/javascripts/videojs/vjs.youtube.js
@@ -27,7 +27,7 @@
     init: function(player, options, ready) {
       // Save this for internal usage
       this.player_ = player;
-      
+
       // No event is triggering this for YouTube
       this['featuresProgressEvents'] = false;
       this['featuresTimeupdateEvents'] = false;
@@ -40,7 +40,7 @@
       this.isAndroid = /(Android)/g.test( navigator.userAgent );
       //used to prevent play events on IOS7 and Android > 4.2 until the user has clicked the player
       this.playVideoIsAllowed = !(this.isIos || this.isAndroid);
-      
+
       // autoplay is disabled for mobile
       if (this.isIos || this.isAndroid) {
         this.player_.options()['autoplay'] = false;
