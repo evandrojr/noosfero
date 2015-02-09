837a8ef9d25a80b9c5aa408ff72d80d0f8fee869
Evandro Junior <evandrojr@gmail.com>
2015-02-09 16:11:50 -0300

    before removeing trailing spaces

--- plugins/video/lib/ext/article.rb
+++ plugins/video/lib/ext/article.rb
@@ -2,8 +2,8 @@ require_dependency 'article'
 
 class Article
 
-  scope :video_gallery, :conditions => ["articles.type == 'VideoGallery'"]
-  
+  scope :video_gallery, :conditions => ["articles.type = 'VideoPlugin::VideoGallery'"]
+
   #FIXME This should be done via hotspot
   def self.folder_types_with_video
     self.folder_types_without_video << 'VideoPlugin::VideoGallery'
@@ -13,31 +13,15 @@ class Article
   class << self
     alias_method_chain :folder_types, :video
   end
-  
 
-#  def self.articles_columns
-#    Article.column_names.map {|c| "articles.#{c}"} .join(",")
-#  end
-#
-#  def self.most_accessed(owner, limit = nil)
-#    conditions = owner.kind_of?(Environment) ?  ["hits > 0"] : ["profile_id = ? and hits > 0", owner.id]
-#    result = Article.relevant_content.find(
-#      :all,
-#      :order => 'hits desc',
-#      :limit => limit,
-#      :conditions => conditions)
-#    result.paginate({:page => 1, :per_page => limit})
-#  end  
-  
-  def self.can_be_listed
-    conditions = owner.kind_of?(Environment) ?  ["hits > 0"] : ["profile_id = ? and hits > 0", owner.id]
+  def self.owner_video_galleries(owner)
+    conditions = owner.kind_of?(Environment) ?  [] : ["profile_id = ?", owner.id]
     result = Article.video_gallery.find(
       :all,
-      :order => 'hits desc',
+      :order => 'created_at desc',
       :conditions => conditions)
   end
-  
-  
+
 end
 
 
