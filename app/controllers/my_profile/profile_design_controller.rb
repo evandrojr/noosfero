97944aa9811bfdddea4dca25d989bd18f166de5c
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-02-25 12:15:38 -0300

    hotspot for stable

--- app/controllers/my_profile/profile_design_controller.rb
+++ app/controllers/my_profile/profile_design_controller.rb
@@ -8,7 +8,8 @@ class ProfileDesignController < BoxOrganizerController
 
   def protect_fixed_block
     block = boxes_holder.blocks.find(params[:id].gsub(/^block-/, ''))
-    if block.fixed && !current_person.is_admin?
+    if !block.nil? && block.fixed && !current_person.is_admin?
+
       render_access_denied
     end
   end
