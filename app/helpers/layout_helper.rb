bb9c8e9963f7e15a42c5be0802f50feef00abecb
Victor Costa <vfcosta@gmail.com>
2015-03-10 18:08:39 -0300

    Move block store css to a separated file

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -49,6 +49,7 @@ module LayoutHelper
       'selectordie',
       'inputosaurus',
       'chat',
+      'block-store',
       pngfix_stylesheet_path,
     ] + tokeninput_stylesheets
     plugins_stylesheets = @plugins.select(&:stylesheet?).map { |plugin| plugin.class.public_path('style.css') }
