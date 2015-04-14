8d285aca7b044df11bcc8ee657769e3599f0f020
Victor Costa <vfcosta@gmail.com>
2015-04-14 15:41:47 -0300

    oauth_client: force return to home

--- plugins/oauth_client/lib/oauth_client_plugin.rb
+++ plugins/oauth_client/lib/oauth_client_plugin.rb
@@ -30,7 +30,7 @@ class OauthClientPlugin < Noosfero::Plugin
   end
 
   def body_ending
-    if session.delete(:oauth_client_login)
+    if logged_in? && session.delete(:oauth_client_login)
       current_user.private_token_expired? if current_user.present?
       private_token = current_user.present? ? current_user.private_token : ''
       "<script>oauthPluginCloseLoginPopup(#{logged_in?}, '#{private_token}');</script>"
