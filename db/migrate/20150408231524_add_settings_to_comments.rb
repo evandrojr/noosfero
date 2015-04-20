4c93783287808938873b3ac55cb0f1a98b2f80c6
Evandro Junior <evandrojr@gmail.com>
2015-04-20 17:50:57 -0300

    fix migration

--- db/migrate/20150408231524_add_settings_to_comments.rb
+++ db/migrate/20150408231524_add_settings_to_comments.rb
@@ -1,5 +1,9 @@
 class AddSettingsToComments < ActiveRecord::Migration
-  def change
-    rename_column :comments, :setting, :settings
+  def self.up
+    add_column :comments, :setting, :text unless column_exists?(:comments, :setting)
+  end
+
+  def self.down
+    remove_column :comments, :setting
   end
 end
