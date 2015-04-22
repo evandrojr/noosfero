50e88e2f66016724625f43c82f96e1a7658a3ac0
Evandro Junior <evandrojr@gmail.com>
2015-04-22 12:05:42 -0300

    Revert "Merge branch 'fix_migration' into 'stable'"
    This reverts commit f99f59236b594a7ce10894c97821b1d60364086d, reversing
    changes made to 6736c36dea7396555087da22fe45f001a289a944.

--- db/migrate/20150408231524_add_settings_to_comments.rb
+++ db/migrate/20150408231524_add_settings_to_comments.rb
@@ -1,9 +1,5 @@
 class AddSettingsToComments < ActiveRecord::Migration
-  def self.up
-    add_column :comments, :setting, :text unless column_exists?(:comments, :setting)
-  end
-
-  def self.down
-    remove_column :comments, :setting
+  def change
+    rename_column :comments, :setting, :settings
   end
 end
