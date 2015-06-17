77d9cc0a70556e4798ae55a8840901f71256c8bd
Victor Costa <vfcosta@gmail.com>
2015-06-17 11:00:26 -0300

    Revert "Merge branch 'change_start_and_end_date_to_datetime' into production"
    This reverts commit dca569a4b682f150e8e9e3116ea7b14843651e44, reversing
    changes made to 364e52a3cfbc1b6a0943d99e31dba1880e1111fa.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,14 +3,13 @@ require 'builder'
 
 class Event < Article
 
-  attr_accessible :start_date, :end_date, :link, :address, :start_hour, :start_minute, :presenter
+  attr_accessible :start_date, :end_date, :link, :address
 
   def self.type_name
     _('Event')
   end
 
   settings_items :address, :type => :string
-  settings_items :presenter, :type => :string
 
   def link=(value)
     self.setting[:link] = maybe_add_http(value)
