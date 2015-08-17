856b15e1a4ea801d522acad53e3ed578ba4bbbb7
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-08-17 16:22:49 -0300

    fixing unit tests of ckeditor plugin

--- plugins/ckeditor_content/test/test_helper.rb
+++ plugins/ckeditor_content/test/test_helper.rb
@@ -1,1 +1,5 @@
 require File.dirname(__FILE__) + '/../../../test/test_helper'
+
+def assert_subclass(parent, child)
+  assert_equal parent, child.superclass, "Class #{child} expected to be a subclass of #{parent}"
+end
