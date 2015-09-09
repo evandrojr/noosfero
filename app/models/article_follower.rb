aeeb54f3cff07d475314df53a2d38528d2a4358e
Victor Costa <vfcosta@gmail.com>
2015-09-09 09:22:40 -0300

    Add counter cache for article followers

--- app/models/article_follower.rb
+++ app/models/article_follower.rb
@@ -1,5 +1,16 @@
 class ArticleFollower < ActiveRecord::Base
+
+  extend CacheCounterHelper
+
   attr_accessible :article_id, :person_id, :since
-  belongs_to :article
+  belongs_to :article, :counter_cache => :followers_count
   belongs_to :person
+
+  after_create do |article_follower|
+    ArticleFollower.update_cache_counter(:followers_count, article_follower.article, 1)
+  end
+
+  after_destroy do |article_follower|
+    ArticleFollower.update_cache_counter(:followers_count, article_follower.article, -1)
+  end
 end
