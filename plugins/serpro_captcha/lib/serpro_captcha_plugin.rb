fa513e94f68695a023c7754f854b0df956516d73
Evandro Junior <evandrojr@gmail.com>
2015-09-22 14:36:07 -0300

    communiticating api to plugin ok

--- plugins/serpro_captcha/lib/serpro_captcha_plugin.rb
+++ plugins/serpro_captcha/lib/serpro_captcha_plugin.rb
@@ -1,11 +1,11 @@
 class SerproCaptchaPlugin < Noosfero::Plugin
 
   def self.plugin_name
-    _('Dialoga custom plugin')
+    _('Serpro captcha plugin')
   end
 
   def self.plugin_description
-    _("Provide a plugin to dialoga environment.")
+    _("Provide a plugin to Serpro's captcha infrastructure.")
   end
 
   def self.api_mount_points
